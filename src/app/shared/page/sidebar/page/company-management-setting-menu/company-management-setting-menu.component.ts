4913b7623b8f78f4bc7753481fb42fd589e40d79
fatemeh-ghahi <fatemehsadatghahi@gmail.com>
2022-06-22 18:36:20 +0430

    change icon sidebar change background sidebar set eventemiter for menu sidebar

--- src/app/shared/page/sidebar/page/company-management-setting-menu/company-management-setting-menu.component.ts
+++ src/app/shared/page/sidebar/page/company-management-setting-menu/company-management-setting-menu.component.ts
@@ -1,3 +1,4 @@
+import { SidebarControleServiceService } from './../../../../service/sidebarControleService/sidebar-controle-service.service';
 import { sidebarState } from './../../../../model/sidebarModel';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,17 +12,21 @@ export class CompanyManagementSettingMenuComponent implements OnInit {
 
   public state: sidebarState = 0;
 
-  constructor() { }
+  constructor(private sidebarControleServiceService:SidebarControleServiceService) {
+    this.sidebarControleServiceService.state.subscribe((res: sidebarState)=>{
+      this.state = res;
+    })
+   }
 
   ngOnInit(): void {
   }
 
   public openSubmenue(state: sidebarState){
     if (this.state != 4) {
-      this.state = state;
+      this.sidebarControleServiceService.state.emit(state);
     }
     else if (this.state == 4){
-      this.state = 0;
+      this.sidebarControleServiceService.state.emit(0);
     }
   }
 
--- src/app/shared/page/sidebar/page/user-management-setting-menu/user-management-setting-menu.component.ts
+++ src/app/shared/page/sidebar/page/user-management-setting-menu/user-management-setting-menu.component.ts
@@ -1,3 +1,4 @@
+import { SidebarControleServiceService } from './../../../../service/sidebarControleService/sidebar-controle-service.service';
 import { sidebarState } from './../../../../model/sidebarModel';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,17 +12,22 @@ export class UserManagementSettingMenuComponent implements OnInit {
 
   public state: sidebarState = 0;
 
-  constructor() { }
+  constructor(private sidebarControleServiceService:SidebarControleServiceService) {
+    this.sidebarControleServiceService.state.subscribe((res: sidebarState)=>{
+      this.state = res;
+    })
+   }
 
   ngOnInit(): void {
   }
 
   public openSubmenue(state: sidebarState){
     if (this.state != 2) {
-      this.state = state;
+      this.sidebarControleServiceService.state.emit(state);
     }
     else if (this.state == 2){
-      this.state = 0;
+      this.sidebarControleServiceService.state.emit(0);
     }
   }
+
 }
