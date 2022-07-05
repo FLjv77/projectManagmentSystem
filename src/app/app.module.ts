c8f55be3c679d481441ec565a3fc6774bd5abdab
fatemeh-ghahi <fatemehsadatghahi@gmail.com>
2022-07-05 12:50:44 +0430

    ad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from "./module/shared/shared.module";
 import { AuthModule } from "./module/auth/auth.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InformationActivityComponent } from './createProjectProcess/page/activity/information-activity/information-activity.component';
-import { ActivityRelationshipsComponent } from './createProjectProcess/page/activity/information-activity/page/activity-relationships/activity-relationships.component';
-import { DependencyActivitiesComponent } from './createProjectProcess/page/activity/information-activity/page/dependency-activities/dependency-activities.component';
-import { ReportActivitiesComponent } from './createProjectProcess/page/activity/information-activity/page/report-activities/report-activities.component';
-import { FinancialReportActivityComponent } from './createProjectProcess/page/activity/information-activity/page/report-activities/page/financial-report-activity/financial-report-activity.component';
-import { ProgressReportActivityComponent } from './createProjectProcess/page/activity/information-activity/page/report-activities/page/progress-report-activity/progress-report-activity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InformationActivityComponent,
-    ActivityRelationshipsComponent,
-    DependencyActivitiesComponent,
-    ReportActivitiesComponent,
-    FinancialReportActivityComponent,
-    ProgressReportActivityComponent,
-
   ],
   imports: [
     BrowserModule,
