b838a6552687468e53238f1713e31400dafc8068
FLjv77 <f.s.lajevardi.g@gmail.com>
2022-07-14 15:34:10 +0430

    FLjv-add-change-role

--- src/app/module/user-management/user-management-routing.module.ts
+++ src/app/module/user-management/user-management-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatUsersComponent } from 'src/app/userManagement/page/chat-users/chat-users.component';
 import { RoleManagementUsersComponent } from 'src/app/userManagement/page/role-management-users/role-management-users.component';
 import { VeiwUserListComponent } from 'src/app/userManagement/page/veiw-user-list/veiw-user-list.component';
+import {RoleManagementOrganizationComponent} from "../../userManagement/page/role-management-organization/role-management-organization.component";
 
 const routes: Routes = [
-  {path: 'userList', component: VeiwUserListComponent  },
+  {path: 'userList', component: VeiwUserListComponent},
+  {path: 'roleManagementUserOrganization', component: RoleManagementOrganizationComponent},
   {path: 'roleManagementUser', component: RoleManagementUsersComponent },
   {path: 'chatUsers', component: ChatUsersComponent },
 ];
