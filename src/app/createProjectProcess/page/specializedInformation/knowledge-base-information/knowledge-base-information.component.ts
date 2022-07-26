59ec8a2d6027adca0e8706a0d46fab38ac372738
fatemeh-ghahi <fatemehsadatghahi@gmail.com>
2022-07-26 21:47:18 +0430

    form

--- src/app/createProjectProcess/page/specializedInformation/knowledge-base-information/knowledge-base-information.component.ts
+++ src/app/createProjectProcess/page/specializedInformation/knowledge-base-information/knowledge-base-information.component.ts
@@ -1,15 +1,37 @@
+import { Router } from '@angular/router';
+import { InputCustomStyle } from './../../../../shared/page/component/input-style/input-style.component';
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-knowledge-base-information',
   templateUrl: './knowledge-base-information.component.html',
-  styleUrls: ['./knowledge-base-information.component.scss']
+  styleUrls: ['./knowledge-base-information.component.scss','../../project-creation-levels/project-creation-levels.component.scss',
+  '../specialized-information-removal-marginalization/specialized-information-removal-marginalization.component.scss']
 })
 export class KnowledgeBaseInformationComponent implements OnInit {
 
-  constructor() { }
+  public inputCustomStyle: InputCustomStyle;
+  public numberCompany = new FormControl();
+  public AreaOfExpertise = new FormControl();
+  public numberAmount = new FormControl();
+  public loanAmount = new FormControl();
+  public reasonAmount = new FormControl();
+  public LocationFormControl = new FormControl();
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.initInputStyle();
   }
 
+  private initInputStyle() {
+    this.inputCustomStyle = new InputCustomStyle(
+      '#AEAEAE', '#AEAEAE', '#AEAEAE'
+    )
+  }
+
+  public goOnMap() {
+    this.router.navigate(['../../createProject/selectLocationOnMap']);
+  }
 }
--- src/app/createProjectProcess/page/specializedInformation/social-transformation-information/social-transformation-information.component.ts
+++ src/app/createProjectProcess/page/specializedInformation/social-transformation-information/social-transformation-information.component.ts
@@ -1,15 +1,32 @@
+import { InputCustomStyle } from './../../../../shared/page/component/input-style/input-style.component';
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-social-transformation-information',
   templateUrl: './social-transformation-information.component.html',
-  styleUrls: ['./social-transformation-information.component.scss']
+  styleUrls: ['./social-transformation-information.component.scss','../../project-creation-levels/project-creation-levels.component.scss',
+  '../specialized-information-removal-marginalization/specialized-information-removal-marginalization.component.scss']
 })
 export class SocialTransformationInformationComponent implements OnInit {
+
+  public inputCustomStyle: InputCustomStyle;
+  public numberFloors = new FormControl();
+  public Capacity = new FormControl();
+  public numberCamp = new FormControl();
+  public budget = new FormControl();
+  public numberIntroduction = new FormControl();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initInputStyle();
   }
 
+  private initInputStyle() {
+    this.inputCustomStyle = new InputCustomStyle(
+      '#AEAEAE', '#AEAEAE', '#AEAEAE'
+    )
+  }
+  
 }
