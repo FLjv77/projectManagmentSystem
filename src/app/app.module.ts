fc8bd56fde076ab383498a174b0c3819e20870f7
fatemeh-ghahi <fatemehsadatghahi@gmail.com>
2022-07-27 13:53:28 +0430

    1111

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,6 @@ import { SharedModule } from "./module/shared/shared.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgxEchartsModule} from "ngx-echarts";
 import {AuthModule} from "./module/auth/auth.module";
-import { WaterInformationWellTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-well-type/water-information-well-type.component';
-import { WaterInformationFountainTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-fountain-type/water-information-fountain-type.component';
-import { WaterInformationDamTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-dam-type/water-information-dam-type.component';
-import { WaterInformationDistributionNetworkTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-distribution-network-type/water-information-distribution-network-type.component';
-import { WaterInformationSourceTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-source-type/water-information-source-type.component';
-import { WaterInformationPompTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-pomp-type/water-information-pomp-type.component';
-import { WaterInformationPurificationTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-purification-type/water-information-purification-type.component';
-import { WaterInformationWaterSealTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-water-seal-type/water-information-water-seal-type.component';
-import { WaterInformationTransferLineTypeComponent } from './createProjectProcess/page/specializedInformation/specialized-information-rural-water/page/water-information-transfer-line-type/water-information-transfer-line-type.component';
-
 
 @NgModule({
   declarations: [
--- src/app/createProjectProcess/page/specializedInformation/specialized-information-in-the-of-food-safety/specialized-information-in-the-of-food-safety.component.ts
+++ src/app/createProjectProcess/page/specializedInformation/specialized-information-in-the-of-food-safety/specialized-information-in-the-of-food-safety.component.ts
@@ -1,19 +1,19 @@
-import { InputCustomStyle } from './../../../../shared/page/component/input-style/input-style.component';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
+import { InputCustomStyle } from './../../../../shared/page/component/input-style/input-style.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-revival-of-industry-information',
-  templateUrl: './revival-of-industry-information.component.html',
-  styleUrls: ['./revival-of-industry-information.component.scss','../../project-creation-levels/project-creation-levels.component.scss',
+  selector: 'app-specialized-information-in-the-of-food-safety',
+  templateUrl: './specialized-information-in-the-of-food-safety.component.html',
+  styleUrls: ['./specialized-information-in-the-of-food-safety.component.scss','../../project-creation-levels/project-creation-levels.component.scss',
   '../specialized-information-removal-marginalization/specialized-information-removal-marginalization.component.scss']
 })
-export class RevivalOfIndustryInformationComponent implements OnInit {
-
+export class SpecializedInformationInTheOfFoodSafetyComponent implements OnInit {
   public inputCustomStyle: InputCustomStyle;
-  public amountGrantedFacilities = new FormControl();
-  public numberIndustries = new FormControl();
+  public centerTitle = new FormControl();
+  public number = new FormControl();
+  public budget = new FormControl();
   public LocationFormControl = new FormControl();
 
   constructor(private router:Router) { }
