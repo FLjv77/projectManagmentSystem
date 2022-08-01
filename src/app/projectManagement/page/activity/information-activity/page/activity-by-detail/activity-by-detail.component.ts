f1ab4c3f752ef55396b856c914f8b640fc0b091b
FLjv77 <f.s.lajevardi.g@gmail.com>
2022-08-01 12:34:23 +0430

    add-edit-activity

--- src/app/projectManagement/page/activity/information-activity/page/activity-by-detail/activity-by-detail.component.ts
+++ src/app/projectManagement/page/activity/information-activity/page/activity-by-detail/activity-by-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { InputCustomStyle } from '../../../../../../shared/page/component/input-style/input-style.component';
 
 @Component({
   selector: 'app-activity-by-detail',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-by-detail.component.scss','../activity-relationships/activity-relationships.component.scss']
 })
 export class ActivityByDetailComponent implements OnInit {
+  public weightTimeControl: FormControl;
+  public weightCostControl: FormControl;
+  public activityNameControl: FormControl;
+  public inputCustomStyle: InputCustomStyle;
+  public isEditMode: boolean = false;
+  public panelOpenState: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initFormControl();
+    this.initInputStyle();
   }
 
+  private initInputStyle() {
+    this.inputCustomStyle = new InputCustomStyle(
+      '#AEAEAE', '#AEAEAE', '#AEAEAE'
+    )
+  }
+
+  private initFormControl() {
+    this.weightCostControl = new FormControl();
+    this.weightTimeControl = new FormControl();
+    this.activityNameControl = new FormControl();
+  }
+
+  public changeEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+
+public togglePanel() {
+    this.panelOpenState = !this.panelOpenState
+}
 }
