bb74fd05e523af55d92266895ac938fa304b80fc
fatemeh-ghahi <fatemehsadatghahi@gmail.com>
2022-09-01 10:15:17 +0430

    111

--- src/app/managementReport/service/reportConnectionToApi/report-connection-to-api.service.ts
+++ src/app/managementReport/service/reportConnectionToApi/report-connection-to-api.service.ts
@@ -1,3 +1,4 @@
+import { ProgressReportDTO } from './../../model/modelDtoAllocationReport';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -18,5 +19,9 @@ export class ReportConnectionToApiService {
 
   public allocationReportVerification(requestAllocationVerification: RequestAllocationVerificationDTO): Observable<ApiResult<boolean>> {
     return this.http.post<ApiResult<boolean>>(url.allocationReportVerification + requestAllocationVerification.allocationReportId, requestAllocationVerification.allocationVerificationDTO);
+  }
+
+  public RegisterProgressReport(projectId: string, progressReportDTO: ProgressReportDTO): Observable<ApiResult<string>>{
+    return this.http.post<ApiResult<string>>(url.RegisterProgressReport + projectId , progressReportDTO);
   }
 }
